51c54e2a17c9aeb7bb4da8ca80235e01655767e7
Moni Gomez <moniggg05@gmail.com>
2026-05-01 08:36:18 +0000

    chore(backend): update config, opensky CRUD, pyproject, export script

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,9 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     OPENSKY_USERNAME: str
     OPENSKY_PASSWORD: str
-
+    SUPABASE_URL: str  
+    SUPABASE_KEY: str  
     class Config:
-        env_file = "backend/.env"
+        env_file = ".env"
 
 
 settings = Settings()
